208e80af7960aa380d88e6986e7d177f56ca449d
Anish Govind <anishg24@gmail.com>
2020-06-28 23:09:17 -0700

    Initial commit. Included seperate file, index2.js that is in my hopes to remove dependency on index.js provided by a tutorial.

---
+++ model/model.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+
+(X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+input_shape = [28, 28, 1]
+
+X_train = tf.reshape(X_train, [X_train.shape[0]] + input_shape)
+X_test = tf.reshape(X_test, [X_test.shape[0]] + input_shape)
+
+X_train = tf.cast(X_train, dtype=tf.float32)
+X_test = tf.cast(X_test, dtype=tf.float32)
+
+X_train /= 255
+X_test /= 255
+
+y_train = tf.reshape(y_train, [-1, 1])
+y_test = tf.reshape(y_test, [-1, 1])
+
+encoder = OneHotEncoder(sparse=False)
+
+y_train = tf.convert_to_tensor(encoder.fit_transform(y_train))
+y_test = tf.convert_to_tensor(encoder.fit_transform(y_test))
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Conv2D(32, (3,3), activation="relu", kernel_initializer="he_uniform", input_shape=input_shape))
+model.add(tf.keras.layers.MaxPooling2D((2,2)))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(100, activation="relu", kernel_initializer="he_uniform"))
+model.add(tf.keras.layers.Dense(10, activation="softmax"))
+
+opt = tf.keras.optimizers.SGD(lr=0.01, momentum=0.9)
+model.compile(optimizer=opt, loss="categorical_crossentropy", metrics=["accuracy"])
+
+h = model.fit(x=X_train, y=y_train, epochs=10, validation_data=(X_test, y_test), batch_size=32)
+
+model.save("model.h5")
